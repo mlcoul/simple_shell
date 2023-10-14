cbd39d54b9e874e706637e302d6f841eed3af995
lamine Coulibaly <mohd.coul@outlook.com>
2023-10-14 05:38:29 -0700

    lam part advance task

---
+++ split_commands.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+
+/**
+ * split_commands - Splits the input based on ;, &&, and ||.
+ * @command: The command string to be split.
+ * Return: An array of strings, each string being a command.
+ */
+char **split_commands(char *command)
+{
+	/* Placeholder for the resulting command array */
+	char **commands = NULL;
+
+	/* Indicate that the command parameter is intentionally unused (for now) */
+	(void)command;
+
+	/* TODO: Actual implementation for splitting commands */
+	return (commands);
+}
+
